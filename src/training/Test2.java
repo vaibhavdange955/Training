c1cd1d8b6a7e7c1bf0f8f2dfe0b206f2b3fb489b
Umesh <Umesh@DESKTOP-V2K7UIN>
2023-07-21 19:29:07 +0530

    commit

--- src/training/Test2.java
+++ src/training/Test2.java
@@ -1,5 +1,25 @@
 package training;
 
 public class Test2 {
+	
+	public static void main(String[] args) {
+		
+		int i = 10; 
+
+//		int int = 20;  We cannot use keyword or datatype to variable name
+
+//		int goto = 30;  We cannot use keyword or datatype to variable name
+
+//		int default = 40;  We cannot use keyword or datatype to variable name
+
+//		int class = 50;  We cannot use keyword or datatype to variable name
+
+		int j = 70;
+		
+		System.out.println(i);
+		System.out.println(j);
+	}
+
+	
 
 }
