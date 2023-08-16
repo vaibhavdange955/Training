b06d332e09353ecc059a06c61b74e2f3198111d1
Umesh <Umesh@DESKTOP-V2K7UIN>
2023-08-16 18:12:43 +0530

    Commit

---
+++ src/training/ReverseNumber.java
@@ -0,0 +1,24 @@
+package training;
+import java.util.Scanner;
+
+public class ReverseNumber {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Number: ");
+		int num =scan.nextInt();
+		
+		int rev=0;
+		int rem=0;
+		
+		while(num !=0) {
+			rem=num%10;
+			rev=rev*10+rem;
+			num=num/10;
+			
+		}
+		System.out.println(rev);	
+				}
+
+}
