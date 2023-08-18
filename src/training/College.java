0beb32a8655f8dad5567980196820d0c0486d9a6
Umesh <Umesh@DESKTOP-V2K7UIN>
2023-08-18 09:26:14 +0530

    commit

---
+++ src/training/College.java
@@ -0,0 +1,39 @@
+package training;
+
+public class College {
+	public static void main(String[] args) {
+		
+		Student s1 =new Student();
+		s1.name="Ashish";
+		s1.age=25;
+		s1.course="Java";
+		
+		
+		Student s2 =new Student();
+		s2.name="Omkar";
+		s2.age=25;
+		s2.course="Java";
+		
+		Student s3 =new Student();
+		s3.name="Vaibhav";
+		s3.age=25;
+		s3.course="Java";
+		
+		Student s4 =new Student();
+		s4.name="Jhon";
+		s4.age=25;
+		s4.course="Java";
+		
+		Student s5 =new Student();
+		s5.name="Swarjit";
+		s5.age=25;
+		s5.course="Java";
+		
+		s1.Info();
+		s2.Info();
+		
+	}
+
+	
+	
+}
---
+++ src/training/Student.java
@@ -0,0 +1,20 @@
+package training;
+
+public class Student {
+	
+
+		
+		String name;
+		int age;
+		String course;
+		static String clgname="MIT";
+		
+		public void Info() {
+			
+			System.out.println(name);
+			System.out.println(age);
+			System.out.println(course);
+			System.out.println(clgname);
+		
+	}
+}
